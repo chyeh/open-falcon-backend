ce5f275b1fd157640fd755a1b02ca023f53bf717
chyeh <chyeh@cepave.com>
2017-02-16 14:22:47 +0800

    specify table name "owl_name_tag" for `NameTag`

--- common/model/owl/name_tag.go
+++ common/model/owl/name_tag.go
@@ -1,6 +1,10 @@
 package owl
 
 type NameTag struct {
-	Id int16 `json:"id" db:"nt_id"`
+	Id    int16  `json:"id" db:"nt_id"`
 	Value string `json:"value" db:"nt_value"`
+}
+
+func (NameTag) TableName() string {
+	return "owl_name_tag"
 }
